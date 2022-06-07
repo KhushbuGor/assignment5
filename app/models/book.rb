1c31302e6d103e6757209978be833f670e824662
khushbu <khushbu.rajgor@bacancy.com>
2022-06-07 10:26:35 +0530

    fileupload,validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :authors
   has_many_attached :cover_photos
+
+  validates :cover_photos, attached: true, content_type: ['image/png'] ,
+    aspect_ratio: :square
+    validates :preview, attached: true, size: { between: 1.kilobyte..1.megabytes , message: 'is not given between size' }
+                                   
 end
