ab8606a5feeb12315a2f688048b4ae96ee1a6fd5
khushbu <khushbu.rajgor@bacancy.com>
2022-06-07 10:24:04 +0530

    fileupload

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ApplicationRecord
   has_one_attached :profile_picture
   validates :profile_picture, attached: true, content_type: ['image/png'] ,
-  aspect_ratio: :square, size: { 1.megabytes, message: 'is not in given size' }
+    aspect_ratio: :square
+    validates :preview, attached: true, size: { between: 1.kilobyte..1.megabytes , message: 'is not given between size' }
+                                   
 end
